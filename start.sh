73fc10d581e079c4ccbf3656f6545ad45e100cd1
DorcasWanjiku <smallwanjiku@gmail.com>
2020-11-02 23:53:26 +0300

    add changes on the start file

--- start.sh
+++ start.sh
@@ -1,1 +1,6 @@
+export SECRET_KEY = 'sh3k1l33'
+export SQLALCHEMY_DATABASE_URI = postgresql+psycopg2://moringa:12345@localhost/pitches
+export DATABASE_URL_DEV=postgresql+psycopg2://moringa:12345@localhost/pitches
+
+
 python3.6 manage.py server
